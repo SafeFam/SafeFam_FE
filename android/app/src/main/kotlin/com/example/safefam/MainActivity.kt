d7a063106621a1ce161d0f856f66432f17b9d050
ch0412 <pce0412010@gmail.com>
2026-07-19 13:21:00 +0900

    feat: implement kakao login Flutter SDK integration (#25)

--- android/app/src/main/kotlin/com/example/safefam/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/safefam/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.example.safefam
 
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import java.security.MessageDigest
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        printKeyHash()
+    }
+
+    private fun printKeyHash() {
+        try {
+            val info = packageManager.getPackageInfo(
+                packageName,
+                PackageManager.GET_SIGNING_CERTIFICATES
+            )
+            for (signature in info.signingInfo?.apkContentsSigners ?: emptyArray()) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val keyHash = Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                Log.d("KeyHash", "KeyHash: $keyHash")
+            }
+        } catch (e: Exception) {
+            Log.e("KeyHash", "Error: $e")
+        }
+    }
+}
